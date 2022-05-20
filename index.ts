9d42111f2df4c11acf771e10c7a09e64a3e3801b
emremsr <emremsr95@gmail.com>
2022-05-20 13:15:09 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
+import { Observable, Subscriber } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
-const users = ajax<any>('https://random-data-api.com/api/name/random_name');
+const helloButton = document.querySelector('#hello');
 
-users.subscribe((data) => console.log(data.response.first_name));
+const HelloClick = new Observable<MouseEvent>((Subscriber) => {
+  helloButton.addEventListener('click', (event) => {
+    Subscriber.next(event as MouseEvent);
+  });
+});
+
+HelloClick.subscribe((event) =>
+  console.log('Sub 1 :', event.type, event.x, event.y)
+);
+
+HelloClick.subscribe((event) =>
+  console.log('Sub  :', event.type, event.x, event.y)
+);
